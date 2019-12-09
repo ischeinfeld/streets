cadc0779a2b31621aa6802702dc7ab2839b5e645
Patrick DeMichele <patd998@gmail.com>
2019-12-08 17:25:36 -0800

    new plot2d stuff, changed .gitignore

--- plot2d.py
+++ plot2d.py
@@ -1,9 +1,12 @@
+from sklearn.linear_model import Lasso
+
 f = open("streets128D.emb")
 x = []
 y = []
-N = 24
+#N = 24
 ids = []
 vecs = {}
+vals = {}
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,10 +14,10 @@ for line in f:
     if len(line.split(" ")) <= 2:
         continue
     id, vec = line.split(" ")[0], line.split(" ")[1:]
-    vec = np.array(map(float,vec)).astype(float)
+    vec = np.array(list(map(float,vec)))
     ids.append(id)
-    vecs[id] =vec 
-
+    vecs[id] = vec 
+    vals[id] = id # appending id as a y-value for now, just for the sake of getting Lasso code working
 
 
 
@@ -27,7 +30,7 @@ rand = random.choice(ids)
 print(rand)
 sims = []
 for vec in vecs:
-    print(vecs[rand])
+    # print(vecs[rand])
     top = vecs[rand].dot(vecs[vec]) 
     bottom = (np.linalg.norm(vecs[vec]) * np.linalg.norm(vecs[rand]))
     sim = top/bottom
@@ -39,10 +42,16 @@ for vec in vecs:
         x2.append(vecs[vec][0])
         y2.append(vecs[vec][1])
 
-plt.hist(sims)
-plt.show()
+#plt.hist(sims)
+#plt.show()
 
 
+
+# Lasso time
+lasso = Lasso(alpha = 0.01, max_iter = 1e5)
+keys = vecs.keys()
+lasso.fit([vecs[key] for key in keys], [vals[key] for key in keys])
+print(lasso.coef_)
 
 
 
